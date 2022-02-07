e87090dad7e145d3a05ce87f02217f355f591d26
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-07 15:53:44 -0500

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
-}
+  const inputform = document.querySelector("form");
+  inputform.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("#searchByID");
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
